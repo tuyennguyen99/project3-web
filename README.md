ba3db364468dbacefbaa53ef6e4eca6e80a10d3f
Jessi <nguyentuyen95.bt@gmail.com>
2023-04-16 02:06:48 +0700

    Add or update the Azure App Service build and deployment workflow config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 You are charged hourly for every VM running. When calculating costs, you should keep this in mind as it would be Number of VMs x Number of Hours Running = Cost
 psql --host=prj3db.postgres.database.azure.com --port=5432 --username=prj3admin@prj3db --dbname=postgres
-
+dbname='techconfdb' user='prj3admin@prj3db' host='prj3db.postgres.database.azure.com' password='@1995N89y12345' port='5432' sslmode='true'
 @1995N89y12345
--- config.py
+++ config.py
@@ -5,13 +5,20 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     DEBUG = True
     POSTGRES_URL="prj3db.postgres.database.azure.com"  #TODO: Update value
+    # POSTGRES_URL="localhost"  #TODO: Update value
+
     POSTGRES_USER="prj3admin@prj3db" #TODO: Update value
+    # POSTGRES_USER="postgres" #TODO: Update value
+
     POSTGRES_PW="@1995N89y12345"   #TODO: Update value
+    # POSTGRES_PW=""   #TODO: Update value
+
     POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_NAMESPACE ='prj3servicebus01.servicebus.windows.net'
     SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://prj3servicebus01.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=3tm8RiwTdNzMQt6n7jvpavtgyrmIaU4KQ+ASbBZuC+s=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
