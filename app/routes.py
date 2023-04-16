8a5aefe2e9684ad11f4eb15e37631ebe4dd1c065
tuyennguyen99 <nguyentuyen95.bt@gmail.com>
2023-04-17 03:11:46 +0700

    Change to send

--- app/routes.py
+++ app/routes.py
@@ -82,7 +82,7 @@ def notification():
             # db.session.commit()
             # TODO Call servicebus queue_client to enqueue notification ID
             sender = queue_client.get_sender()
-            sender.queue_message(Message(str(notification.id)))
+            sender.send(Message(str(notification.id)))
 
             #################################################
             ## END of TODO
